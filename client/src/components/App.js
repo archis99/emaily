96ba01f11140f6e9eae97c66418b3c5363956ac3
archis99 <archismansahoo8@gmail.com>
2018-03-19 22:04:12 +0530

    materialized Header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,25 @@
 //Rendering Layer Control
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
     return(
-        <div>
-            Hi There
+        //container is a materialize css class
+        <div className="container"> 
+             {/* BrowserRouter component expects at most 1 childcomponent */}
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact={true} path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
